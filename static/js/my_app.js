191f0f9b580a3862fa63c3a9a07fda42c46a707f
CaptBoscho <corbin.byers@gmail.com>
2016-12-03 18:08:08 +0000

    ugh

--- static/js/my_app.js
+++ static/js/my_app.js
@@ -1,7 +1,10 @@
 angular.module('myApp', [])
  .controller('myController', ['$scope', '$http', 
                               function($scope, $http) {
-
+    $scope.urls = [];
+    $scope.titles = [];
+    $scope.names = [];
+    $scope.images = [];
     console.log("in my app");
     $http.get('/user/profile')
         .success(function(data, status, headers, config) {
@@ -15,12 +18,17 @@ angular.module('myApp', [])
 
     $scope.addProj = function() {
        console.log("in project");
+
        var newUrl = {
          "name" : $scope.name,
          "title" : $scope.title,
          "foto" : $scope.foto,
          "url" : $scope.url
        };
+
+       if(newUrl.foto == '' || newUrl.foto == undefined){
+         newUrl.foto = "http://shushi168.com/data/out/123/36699123-images.png";
+       }
        $scope.user.urls += newUrl;
        console.log($scope.user._id);
        console.log($scope.user.urls.length);
@@ -30,6 +38,10 @@ angular.module('myApp', [])
          });
 
        console.log(newUrl);
+       $scope.urls += newUrl.url;
+       $scope.names += newUrl.name;
+       $scope.titles += newUrl.title;
+       $scope.images += newUrl.images;
       // $http.put('/user/' + $scope.user._id + 'updateUrl', newUrl).success(function(data){
         //  console.log('success');
       // }); 
